1baeb79d84b24e8c15edf7eed7f924acb8b0c91a
mijaelrcf <mijael.rcf@gmail.com>
2023-07-05 18:37:11 -0400

    Configuring injections

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstDotnetAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HelloWorldController : ControllerBase
+{
+    IHelloWorldService helloWorldService;
+
+    public HelloWorldController(IHelloWorldService helloWorldService)
+    {
+        this.helloWorldService = helloWorldService;
+    }
+
+    public IActionResult Get()
+    {
+        return Ok(helloWorldService.GetHelloWorld());
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IHelloWorldService, HelloWorldService>(); // Es una buena practica
+//builder.Services.AddScoped<IHelloWorldService>(p => new HelloWorldService()); // Es otra forma de inyectar pero no es muy comun 
 
 var app = builder.Build();
 
@@ -22,7 +24,7 @@ app.UseAuthorization();
 
 //app.UseWelcomePage();
 
-app.UseTimeMiddleware();
+//app.UseTimeMiddleware();
 
 app.MapControllers();
 
